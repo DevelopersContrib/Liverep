40069033a70d3f39e57095e4c8131f836ea16d59
Larry C. Parba <larz.parba@gmail.com>
2017-06-19 13:43:42 +0800

    #12 updating js files

--- index.js
+++ index.js
@@ -260,9 +260,9 @@ socket.on('update user', function(firstname,lastname,username,password,profile_i
 	});
 
 	// var $query = "UPDATE Members SET FirstName = ?, LastName = ?, Username = ?, Password = ?, profile_image = ? WHERE MemberId = ?";
-	// { LastName: lastname }, { Username: username }, { Password: password }, { profile_image: profile_image },
+	// 
 	
-	connection.query('UPDATE Members SET ? WHERE ?', [{ FirstName: firstname },  { MemberId: userid }])
+	connection.query('UPDATE Members SET ? SET ? SET ? SET ? SET ? WHERE ?', [{ FirstName: firstname }, { LastName: lastname }, { Username: username }, { Password: password }, { profile_image: profile_image }, { MemberId: userid }])
 	connection.end();
 });
 
